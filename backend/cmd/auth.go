dd149a203c6b551e2ed90294a78d58b123adac16
yyyushiro <yushiro0418@gmail.com>
2026-04-16 15:42:17 -0400

    feat(middleware): implement middleware authorizing the current user.

--- backend/cmd/auth.go
+++ backend/cmd/auth.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"context"
 	"crypto/rand"
 	"encoding/base64"
+	"fmt"
+	"net/http"
 	"os"
 	"strconv"
 	"time"
@@ -40,4 +43,84 @@ func makeSignedAccessToken(sub uuid.UUID) (string, error) {
 		return "", err
 	}
 	return signedAccessToken, nil
+}
+
+type accessTokenClaims struct {
+	Sub string `json:"sub"`
+	jwt.RegisteredClaims
+}
+
+func verifyAccessTokenJWT(tokenString string, secret []byte) (*accessTokenClaims, error) {
+	var claims accessTokenClaims
+	tok, err := jwt.ParseWithClaims(tokenString, &claims, func(t *jwt.Token) (interface{}, error) {
+		if _, ok := t.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("unexpected alg: %v", t.Header["alg"])
+		}
+		return secret, nil
+	})
+	if err != nil {
+		return nil, err
+	}
+	if !tok.Valid {
+		return nil, fmt.Errorf("invalid token")
+	}
+	return &claims, nil
+}
+
+// You can expand this struct if you want to get more information from authorization later.
+type AuthResult struct {
+	sub                  uuid.UUID
+	newAccessTokenCookie *http.Cookie
+}
+
+// RequireAuth is used in every API call and verifies the user's access token.
+func RequireAuth(r *http.Request) (*AuthResult, error) {
+	accessToken, err := GetAndVerifyCookie(r, "access_token")
+	if err != nil {
+		return nil, fmt.Errorf("Verifying access token Cookie: %w", err)
+	}
+	// verify accesstoken (jwt)
+	accessTokenClaims, err := verifyAccessTokenJWT(accessToken, []byte(os.Getenv("JWT_SECRET")))
+	if err != nil {
+		return nil, fmt.Errorf("Verifying access token JWT: %w", err)
+	}
+
+	subUuid, err := uuid.Parse(accessTokenClaims.Sub)
+	if err != nil {
+		return nil, fmt.Errorf("parsing string into uuid: %w", err)
+	}
+
+	// verify exp this is happy route
+	if accessTokenClaims.ExpiresAt.Time.Unix() > time.Now().Unix() {
+		return &AuthResult{sub: subUuid, newAccessTokenCookie: nil}, nil
+	}
+
+	// if exp is invalid, then check refreshtoken.
+	refreshToken, err := GetAndVerifyCookie(r, "refresh_token")
+	if err != nil {
+		return nil, fmt.Errorf("Verifying refresh token Cookie: %w", err)
+	}
+
+	// get sub by redis and refreshtoken if not found, then expired.
+	subStr, err := rdb.Get(context.Background(), refreshToken).Result()
+	if err != nil {
+		return nil, fmt.Errorf("Getting sub from redis: %w", err)
+	}
+
+	subUuid, err = uuid.Parse(subStr)
+	if err != nil {
+		return nil, fmt.Errorf("Parsing sub into uuid: %w", err)
+	}
+
+	// found sub, so issue new access token.
+	newSignedAccessToken, err := makeSignedAccessToken(subUuid)
+	if err != nil {
+		return nil, fmt.Errorf("Making new access token: %w", err)
+	}
+
+	// return the new access token cookie.
+	return &AuthResult{
+		sub:                  subUuid,
+		newAccessTokenCookie: makeSignedCookie("access_token", newSignedAccessToken, 900),
+	}, nil
 }
