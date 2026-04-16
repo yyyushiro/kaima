3a6b98a83f7abfd0d445559b209e19a68bbde0b4
yyyushiro <yushiro0418@gmail.com>
2026-04-15 22:38:08 -0400

    feat(frontend): add TimelinePage

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import TitlePage from './pages/TitlePage.tsx'
+import TimeLinePage from "./pages/TimelinePage.tsx"
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<TitlePage />} />
+        <Route path="/timeline" element={<TimeLinePage />} />
       </Routes>
     </BrowserRouter>
   )
---
+++ frontend/pages/TimelinePage.tsx
@@ -0,0 +1,7 @@
+export default function TimeLinePage() {
+    return (
+        <div>
+            <h1>This is the timeline.</h1>
+        </div>
+    )
+}
